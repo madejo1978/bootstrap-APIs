712fcb02ebec84585d5912b8a58b864ae9613f87
Martin de Jonge <madejo@hotmail.com>
2019-02-15 11:49:41 +0100

    v1.1

--- js/chuck.js
+++ js/chuck.js
@@ -4,6 +4,37 @@
 // function getChuck() {
 //        console.log('getChuck function started');
 
+var jokeClicker = document.getElementById("jokeClicker");
+console.log(jokeClicker);
+
+jokeClicker.addEventListener("click", getJoke);
+function getJoke() {
+
+var xhr = new XMLHttpRequest();
+var url = "https://api.chucknorris.io/jokes/random";
+xhr.onreadystatechange = function () {
+        console.log(xhr.readyState);
+        if (xhr.readyState == 4 && xhr.status == 200) {
+                console.log(xhr.response);
+                var input_api = xhr.response;
+                var chuck_jokes = JSON.parse(input_api);
+                //     console.log(chuck_jokes) ;
+                document.getElementById("chuck-pic").innerHTML = '<img src="' + chuck_jokes.icon_url + '"></img>';
+                document.getElementById("joke").innerHTML = chuck_jokes.value;
+        }
+}
+
+xhr.open("GET", url);
+xhr.send();
+console.log(xhr);
+} 
+
+/* var jokeClicker = document.getElementById("jokeClicker");
+console.log(jokeClicker);
+
+jokeClicker.addEventListener("click", getJoke);
+function getJoke() {
+
         var xhr = new XMLHttpRequest();
         var url = "https://api.chucknorris.io/jokes/random";
         xhr.onreadystatechange = function(){
@@ -21,3 +52,4 @@
         xhr.open("GET",url);                                        
         xhr.send();
         console.log(xhr);
+} */
